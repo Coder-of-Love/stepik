8c50116ea1cf526c68a881e6bd00d222487deaec
Evgenii Minkov <j.m.n.90@bk.ru>
2018-11-23 18:08:56 +0300

    Add files via upload

---
+++ Java. Базовый курс/2.1.3 Примитивные типы.java
@@ -0,0 +1,36 @@
+﻿/*
+Реализуйте метод, возвращающий ответ на вопрос: правда ли, что a + b = c?
+Допустимая погрешность – 0.0001 (1E-4)
+
+Можно использовать класс Math и его методы. Класс Math доступен всегда, импортировать его не надо.
+*/
+
+// мое
+public static boolean doubleExpression(double a, double b, double c) {
+    double 
+        x = a + b,
+        y = c,
+        e = 0.0001d,
+        diff = Math.abs(x - y);
+    return diff < e;
+}
+
+// чужое
+public static boolean doubleExpression(double a, double b, double c) {
+    return Math.abs(a + b - c)<1E-4;
+}
+
+// чужое, хитрое
+public static boolean doubleExpression(double a, double b, double c) {
+    return a*10000 + b*10000 == c*10000;
+}
+
+// чужое, через приведение
+public static boolean doubleExpression(double a, double b, double c) {
+    return (float)(a + b) == (float)c;
+}
+
+// чужое, лучшее время выполнения
+public static boolean doubleExpression(double a, double b, double c) {
+    return a+b>c?a+b-c<1e-4:c-a-b<1e-4;
+}
---
+++ Java. Базовый курс/2.1.4 Примитивные типы.java
@@ -0,0 +1,17 @@
+﻿/*
+Реализуйте метод flipBit, изменяющий значение одного бита заданного целого числа на противоположное. Данная задача актуальна, например, при работе с битовыми полями.
+
+Договоримся, что биты нумеруются от младшего (индекс 1) к старшему (индекс 32).
+*/
+
+// мое
+public static int flipBit(int value, int bitIndex) {
+    int flipValue = (int)Math.pow(2, bitIndex - 1);
+    int result = value ^ flipValue;
+    return result;
+}
+
+// чужое, лучшее время выполнения
+public static int flipBit(int value, int bitIndex) {
+    return value ^ 1 << bitIndex - 1;
+}
